081cca95aa43c5a65f3871cd1762c508c89211fa
Kian <sabbah128@gmail.com>
2025-10-21 22:55:28 +0330

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -3,8 +3,11 @@ from typing import Annotated
 
 
 class Settings (BaseSettings):
-    SQLALCHEMY_DATABASE_URL: str = "sqlite:///../sqlite.db"
-    model_config = SettingsConfigDict(env_file=".env")
+    SQLALCHEMY_DATABASE_URL: str = ""
+    model_config = SettingsConfigDict(env_file="../.env")
 
 
 settings = Settings()
+
+if not settings.SQLALCHEMY_DATABASE_URL:
+    raise ValueError("❌ Environment variable SQLALCHEMY_DATABASE_URL not set or empty.")
